d2df40c5fb62901f42e686967ef68169f6464230
yshinde005 <yashkshinde@gmail.com>
2022-11-14 00:02:39 +0530

    Terminal config. commit

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,44 @@
-use std::io::{self , stdout, Read};
-use termion::raw::IntoRawMode;
+// use std::io::{self, stdout};
+// use termion::event::Key;
+// use termion::input::TermRead;
+// use termion::raw::IntoRawMode;
+#![warn(clippy::all, clippy::pedantic)]
+mod editor;
 
-fn to_ctrl_byte(c: char) -> u8{
-    let byte = c as u8;
-    byte & 0b0001_1111
 
-}
+// fn die(e: std::io::Error) {
+    // panic!("{}",e);
+// }
+// 
+
+mod terminal;
+use editor::Editor;
+pub use terminal::Terminal;
+pub use editor::Position;
 
 fn main() {
-    let _stdout = stdout().into_raw_mode().unwrap();
-    for b in io::stdin().bytes(){
-        let b = b.unwrap();
-        let c = b as char;
-        if c.is_control(){
-            println!("{:?} \r", b);
-        }else {
-            println!("{:?} ({})\r",b , c);
-        }
-        if b == to_ctrl_byte('q'){
-            break;
-        }
-    }
+    // let _stdout = stdout().into_raw_mode().unwrap();
+    Editor::default().run();    
+
+    // for key in io::stdin().keys(){
+    //     match key {
+    //         Ok(key) => match key {
+    //             Key::Char(c) => {
+    //                 if c.is_control(){
+    //                     println!("{:?} \r", c as u8);
+    //                 } else {
+    //                     println!("{:?} ({}) \r", c as u8, c);
+    //                 }
+                    
+
+    //             }
+    //             Key::Ctrl('q') => break,
+    //             _=> println!("{:?}\r", key),
+
+    //         },
+    //         Err(err) => die(err),
+        
+    //     }
+
+    // }
 }
